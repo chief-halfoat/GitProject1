200e5bdd78e0b0cb0c1fe677944e2b1d208d69f5
chief-halfoat <johnathan.j.hart@gmail.com>
2022-05-14 12:06:31 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("First Git Project");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
